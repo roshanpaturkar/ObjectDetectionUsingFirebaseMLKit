e9c06db1207a4311519dfccd1732466a5fc0e96d
roshanpaturkar <paturkarr@gmail.com>
2019-10-10 13:43:50 +0530

    Final Application

---
+++ app/src/main/java/roshan/paturkar/com/objectdetector/MainActivity.kt
@@ -0,0 +1,209 @@
+package roshan.paturkar.com.objectdetector
+
+import android.Manifest
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.app.Activity
+import android.content.ContentValues
+import android.content.Context
+import android.content.pm.PackageManager
+import kotlinx.android.synthetic.main.activity_main.*
+import android.provider.MediaStore
+import android.content.Intent
+import android.graphics.*
+import android.net.Uri
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import com.google.firebase.ml.vision.FirebaseVision
+import com.google.firebase.ml.vision.common.FirebaseVisionImage
+import com.google.firebase.ml.vision.objects.FirebaseVisionObject
+import com.google.firebase.ml.vision.objects.FirebaseVisionObjectDetectorOptions
+
+class MainActivity : AppCompatActivity(),
+    ActivityCompat.OnRequestPermissionsResultCallback {
+
+    companion object {
+        const val ODT_PERMISSIONS_REQUEST: Int = 1
+        const val ODT_REQUEST_IMAGE_CAPTURE = 1
+    }
+
+    private lateinit var outputFileUri: Uri
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        cameraButton.setOnClickListener { _ ->
+            val takePhotoIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            if (takePhotoIntent.resolveActivity(packageManager) != null) {
+                val values = ContentValues()
+                values.put(MediaStore.Images.Media.TITLE, "MLKit_codelab")
+                outputFileUri = getContentResolver()
+                    .insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)!!
+
+                takePhotoIntent.putExtra(MediaStore.EXTRA_OUTPUT, outputFileUri)
+                startActivityForResult(takePhotoIntent, ODT_REQUEST_IMAGE_CAPTURE)
+            }
+        }
+
+        if (ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE) !=
+            PackageManager.PERMISSION_GRANTED) {
+
+            cameraButton.isEnabled = false
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
+                ODT_PERMISSIONS_REQUEST
+            )
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == ODT_REQUEST_IMAGE_CAPTURE &&
+            resultCode == Activity.RESULT_OK) {
+
+            val image = getCapturedImage()
+
+            imageView.setImageBitmap(image)
+
+            runObjectDetection(image)
+        }
+    }
+
+    private fun runObjectDetection(bitmap: Bitmap) {
+
+        val image = FirebaseVisionImage.fromBitmap(bitmap)
+
+        val options = FirebaseVisionObjectDetectorOptions.Builder()
+            .setDetectorMode(FirebaseVisionObjectDetectorOptions.SINGLE_IMAGE_MODE)
+            .enableMultipleObjects()
+            .enableClassification()
+            .build()
+        val detector = FirebaseVision.getInstance().getOnDeviceObjectDetector(options)
+
+        detector.processImage(image)
+            .addOnSuccessListener {
+
+                val drawingView = DrawingView(getApplicationContext(), it)
+                drawingView.draw(Canvas(bitmap))
+                runOnUiThread {
+                    imageView.setImageBitmap(bitmap)
+                }
+            }
+            .addOnFailureListener {
+                Toast.makeText(
+                    baseContext, "Oops, something went wrong!",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+    }
+
+    private fun getCapturedImage(): Bitmap {
+
+        val srcImage = FirebaseVisionImage
+            .fromFilePath(baseContext, outputFileUri).getBitmap()
+
+        val scaleFactor = Math.min(
+            srcImage.width / imageView.width.toFloat(),
+            srcImage.height / imageView.height.toFloat()
+        )
+
+        val deltaWidth = (srcImage.width - imageView.width * scaleFactor).toInt()
+        val deltaHeight = (srcImage.height - imageView.height * scaleFactor).toInt()
+
+        val scaledImage = Bitmap.createBitmap(
+            srcImage, deltaWidth / 2, deltaHeight / 2,
+            srcImage.width - deltaWidth, srcImage.height - deltaHeight
+        )
+        srcImage.recycle()
+        return scaledImage
+
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>, grantResults: IntArray
+    ) {
+        when (requestCode) {
+            ODT_PERMISSIONS_REQUEST -> {
+                if (grantResults.isNotEmpty() && grantResults[0] ==
+                    PackageManager.PERMISSION_GRANTED
+                ) {
+                    cameraButton.isEnabled = true
+                }
+            }
+        }
+    }
+}
+
+class DrawingView(context: Context, var visionObjects: List<FirebaseVisionObject>) : View(context) {
+
+    companion object {
+        val categoryNames: Map<Int, String> = mapOf(
+            FirebaseVisionObject.CATEGORY_UNKNOWN to "Unknown",
+            FirebaseVisionObject.CATEGORY_HOME_GOOD to "Home Goods",
+            FirebaseVisionObject.CATEGORY_FASHION_GOOD to "Fashion Goods",
+            FirebaseVisionObject.CATEGORY_FOOD to "Food",
+            FirebaseVisionObject.CATEGORY_PLACE to "Place",
+            FirebaseVisionObject.CATEGORY_PLANT to "Plant"
+        )
+    }
+
+    val MAX_FONT_SIZE = 96F
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        val pen = Paint()
+        pen.textAlign = Paint.Align.LEFT
+
+        for (item in visionObjects) {
+            pen.color = Color.RED
+            pen.strokeWidth = 8F
+            pen.style = Paint.Style.STROKE
+            val box = item.getBoundingBox()
+            canvas.drawRect(box, pen)
+
+            val tags: MutableList<String> = mutableListOf()
+            tags.add("Category: ${categoryNames[item.classificationCategory]}")
+            if (item.classificationCategory != FirebaseVisionObject.CATEGORY_UNKNOWN) {
+                tags.add("Confidence: ${item.classificationConfidence!!.times(100).toInt()}%")
+            }
+
+            var tagSize = Rect(0, 0, 0, 0)
+            var maxLen = 0
+            var index: Int = -1
+
+            for ((idx, tag) in tags.withIndex()) {
+                if (maxLen < tag.length) {
+                    maxLen = tag.length
+                    index = idx
+                }
+            }
+
+            pen.style = Paint.Style.FILL_AND_STROKE
+            pen.color = Color.YELLOW
+            pen.strokeWidth = 2F
+
+            pen.textSize = MAX_FONT_SIZE
+            pen.getTextBounds(tags[index], 0, tags[index].length, tagSize)
+            val fontSize: Float = pen.textSize * box.width() / tagSize.width()
+
+            if (fontSize < pen.textSize) pen.textSize = fontSize
+
+            var margin = (box.width() - tagSize.width()) / 2.0F
+            if (margin < 0F) margin = 0F
+
+            for ((idx, txt) in tags.withIndex()) {
+                canvas.drawText(
+                    txt, box.left + margin,
+                    box.top + tagSize.height().times(idx + 1.0F), pen
+                )
+            }
+        }
+    }
+}
